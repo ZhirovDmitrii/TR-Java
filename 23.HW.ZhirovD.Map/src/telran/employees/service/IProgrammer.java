90638433d1c906c3c5bca91ac83abf8d5a0107ab
ZhirovDmitrii <130872529+ZhirovDmitrii@users.noreply.github.com>
2023-04-24 00:53:34 +0300

    homework 23

--- 23.HW.ZhirovD.Map/src/telran/employees/service/IProgrammer.java
+++ 23.HW.ZhirovD.Map/src/telran/employees/service/IProgrammer.java
@@ -1,5 +1,16 @@
 package telran.employees.service;
 
+import java.util.List;
+
+import telran.employees.dto.Programmer;
+
 public interface IProgrammer {
-	
+	public boolean addProgrammer(Programmer programmer);
+	public boolean removeProgrammer(int id);
+	public Programmer getProgrammerData(int id);
+	public boolean addNewTechnology(int id, String technology);
+	public boolean removeTechnology(int id, String technology);
+	public List<Programmer> getProgrammersWithTechnology(String technology);
+	public List<Programmer> getProgrammersWithSalaries(int salaryFrom, int salaryTo);
+	public boolean updateSalary(int id, int salary);
 }
--- 23.HW.ZhirovD.Map/src/telran/employees/service/ProgrammersMaps.java
+++ 23.HW.ZhirovD.Map/src/telran/employees/service/ProgrammersMaps.java
@@ -1,5 +1,63 @@
 package telran.employees.service;
 
-public class ProgrammersMaps {
+import java.util.HashMap;
+import java.util.List;
 
+import telran.employees.dto.Programmer;
+
+public class ProgrammersMaps implements IProgrammer{
+	private HashMap<Integer, Programmer> programmers;
+	public ProgrammersMaps(HashMap<Integer, Programmer> programmers) {
+		super();
+		this.programmers = programmers;
+	}
+
+	@Override
+	public boolean addProgrammer(Programmer programmer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeProgrammer(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Programmer getProgrammerData(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addNewTechnology(int id, String technology) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeTechnology(int id, String technology) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Programmer> getProgrammersWithTechnology(String technology) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Programmer> getProgrammersWithSalaries(int salaryFrom, int salaryTo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateSalary(int id, int salary) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
