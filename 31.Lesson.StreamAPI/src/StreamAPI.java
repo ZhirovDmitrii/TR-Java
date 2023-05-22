8c86a738bf98cf1f84c685caa2179615eca3e90e
ZhirovDmitrii <130872529+ZhirovDmitrii@users.noreply.github.com>
2023-05-22 19:54:37 +0300

    Lesson 31

---
+++ 31.Lesson.StreamAPI/src/StreamAPI.java
@@ -0,0 +1,16 @@
+
+public class StreamAPI {
+
+	public static void main(String[] args) {
+		Movie[] array = { 
+				new Movie("a", 2022, 4), 
+				new Movie("b", 2023, 4.5), 
+				new Movie("c", 2022, 4.5),
+				new Movie("d", 2023, 3.5), 
+				new Movie("e", 2023, 5.5), 
+				new Movie("aa", 2023, 5.5) };
+		
+		
+	}
+
+}
