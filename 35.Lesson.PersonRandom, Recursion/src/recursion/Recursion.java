2740f46508f270b95a8548854b0e425e73b08c2e
ZhirovDmitrii <130872529+ZhirovDmitrii@users.noreply.github.com>
2023-06-09 19:36:39 +0300

    Update Recursion.java

--- 35.Lesson.PersonRandom, Recursion/src/recursion/Recursion.java
+++ 35.Lesson.PersonRandom, Recursion/src/recursion/Recursion.java
@@ -6,7 +6,14 @@ public class Recursion {
 		method(3); // 1. non recursion call
 		
 		System.out.println(nonRecursionFactorial(5));
-		System.out.println(recursionFactorial(5));
+		System.out.println(recursionFactorial(0));
+		
+		int[] array = {1,2,3,4,5,6,8,9,10};
+		System.out.println(nonRecBinarySearch(array, 0));
+		System.out.println(nonRecBinarySearch(array, 2));
+		System.out.println(recBinarySearch(array, 0));
+		System.out.println(recBinarySearch(array, 2));
+		
 	}
 
 	public static void method(int x) {
@@ -23,6 +30,43 @@ public class Recursion {
 	}
 
 	public static int recursionFactorial(int x) {
-		return x == 1 ? 1 : x * nonRecursionFactorial(x - 1);
+		return x == 0 ? 1 : x * nonRecursionFactorial(x - 1);
+	}
+	
+	public static int nonRecBinarySearch(int[] array, int x) {
+		int left = 0;
+		int right = array.length -1;
+		int middle;
+		
+		while(left <= right) {
+			middle = (left + right) / 2;
+			if(array[middle] == x)
+				return middle;
+			else if(x > array[middle])
+				left = middle +1;
+			else
+				right = middle - 1;
+		}
+		return -1;
+	}
+	
+	public static int recBinarySearchWork(int[] array, int x, int left, int right) {
+		if(left > right)
+			return -1;
+		
+		int middle = (left + right) / 2;
+		
+		if(array[middle] == x)
+			return middle;
+		else if(x > array[middle])
+			left = middle +1;
+		else
+			right = middle - 1;
+		
+		return recBinarySearchWork(array, x, left, right);
+	}
+	
+	public static int recBinarySearch(int[] array, int x) {
+		return recBinarySearchWork(array, x, 0, array.length-1);
 	}
 }
